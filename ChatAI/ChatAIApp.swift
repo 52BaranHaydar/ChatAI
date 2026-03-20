ed19e19e37b121dac6921cc3c14a39086cf2f82d
Baran Haydar <baranhaydar80@gmail.com>
2026-03-20 22:32:41 +0300

    Initial Commit

---
+++ ChatAI/ChatAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAIApp.swift
+//  ChatAI
+//
+//  Created by Baran on 20.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct ChatAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
